937ad8e335fdb3fcab5c4b4d90ee2a42dcff85f2
Yan Pujante <yan@pongasoft.com>
2020-10-22 08:24:50 -0700

    Project created

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("js") version "1.4.10"
+}
+group = "org.pongasoft"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlinx")
+    }
+}
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.2")
+}
+kotlin {
+    js {
+        browser {
+            binaries.executable()
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+        }
+    }
+}
