a30c811b1c6d5bff82963d733d22e77554cf4028
Yan Pujante <yan@pongasoft.com>
2022-12-27 11:28:23 -0800

    uses RE Edit
    - uses new download_url links

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "org.pongasoft"
-version = "1.2.6"
+version = "1.3.0"
 
 repositories {
     mavenCentral()
--- src/plugin/resources/skeletons/common/cmake/options.cmake
+++ src/plugin/resources/skeletons/common/cmake/options.cmake
@@ -1,30 +1,33 @@
-cmake_minimum_required(VERSION 3.17)
+cmake_minimum_required(VERSION 3.24)
 
 #################
 # re-cmake
 #################
-set(re-cmake_GIT_TAG "v1.4.4" CACHE STRING "re-cmake git tag")
+set(re-cmake_GIT_TAG "v1.5.0" CACHE STRING "re-cmake git tag")
 set(re-cmake_GIT_REPO "https://github.com/pongasoft/re-cmake" CACHE STRING "re-cmake git repository url")
+set(re-cmake_DOWNLOAD_URL "${re-cmake_GIT_REPO}/archive/refs/tags/${re-cmake_GIT_TAG}.zip" CACHE STRING "re-cmake download url" FORCE)
 
 # Uncomment/set to proper value if local instead of remote
 set(re-cmake_ROOT_DIR "") # use remote re-cmake
 #set(re-cmake_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../../pongasoft/re-cmake")
 
 #################
-# re-logging (repo defined by re-cmake but can override here)
+# re-logging
 #################
+set(re-logging_GIT_REPO "https://github.com/pongasoft/re-logging" CACHE STRING "re-logging git repository url")
 set(re-logging_GIT_TAG "v1.0.1" CACHE STRING "re-logging git tag")
-#set(re-logging_GIT_REPO "https://github.com/pongasoft/re-mock" CACHE STRING "re-logging git repository url")
+set(re-logging_DOWNLOAD_URL "${re-logging_GIT_REPO}/archive/refs/tags/${re-logging_GIT_TAG}.zip" CACHE STRING "re-logging download url" FORCE)
 
 # Uncomment/set to proper value if local instead of remote
 set(re-logging_ROOT_DIR "") # use remote re-logging
 #set(re-logging_ROOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../../pongasoft/re-logging")
 
 #################
-# re-mock (repo defined by re-cmake but can override here)
+# re-mock
 #################
-set(re-mock_GIT_TAG "v1.3.0" CACHE STRING "re-mock git tag")
-#set(re-mock_GIT_REPO "https://github.com/pongasoft/re-mock" CACHE STRING "re-mock git repository url")
+set(re-mock_GIT_REPO "https://github.com/pongasoft/re-mock" CACHE STRING "re-mock git repository url")
+set(re-mock_GIT_TAG "v1.3.2" CACHE STRING "re-mock git tag")
+set(re-mock_DOWNLOAD_URL "${re-mock_GIT_REPO}/archive/refs/tags/${re-mock_GIT_TAG}.zip" CACHE STRING "re-mock download url" FORCE)
 
 # Uncomment/set to proper value if local instead of remote
 set(re-mock_ROOT_DIR "") # use remote re-mock
@@ -34,10 +37,11 @@ set(re-mock_ROOT_DIR "") # use remote re-mock
 option(RE_MOCK_SUPPORT_FOR_AUDIO_FILE "" [-options_re_mock_support_for_audio_file-])
 
 #################
-# googletest (tag/repo defined by re-cmake but can override here)
+# googletest
 #################
-#set(googletest_GIT_TAG "<tag goes here>" CACHE STRING "googletest git tag")
-#set(googletest_GIT_REPO "https://github.com/google/googletest" CACHE STRING "googletest git repository URL")
+set(googletest_GIT_REPO "https://github.com/google/googletest" CACHE STRING "googletest git repository URL")
+set(googletest_GIT_TAG "release-1.12.1" CACHE STRING "googletest git tag")
+set(googletest_DOWNLOAD_URL "${googletest_GIT_REPO}/archive/refs/tags/${googletest_GIT_TAG}.zip" CACHE STRING "googletest download url" FORCE)
 
 # Uncomment/set to proper value if local instead of remote
 set(googletest_ROOT_DIR "") # use remote googletest
