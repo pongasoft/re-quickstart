618705447447165cab9783f1ae47bd8c4af8d5f4
Yan Pujante <yan@pongasoft.com>
2023-04-28 10:57:57 -0700

    Uses hashes to harden the build

--- src/plugin/resources/skeletons/common/cmake/options.cmake
+++ src/plugin/resources/skeletons/common/cmake/options.cmake
@@ -3,9 +3,10 @@ cmake_minimum_required(VERSION 3.24)
 #################
 # re-cmake
 #################
-set(re-cmake_GIT_TAG "v1.6.0" CACHE STRING "re-cmake git tag")
+set(re-cmake_GIT_TAG "v1.6.1" CACHE STRING "re-cmake git tag")
 set(re-cmake_GIT_REPO "https://github.com/pongasoft/re-cmake" CACHE STRING "re-cmake git repository url")
 set(re-cmake_DOWNLOAD_URL "${re-cmake_GIT_REPO}/archive/refs/tags/${re-cmake_GIT_TAG}.zip" CACHE STRING "re-cmake download url" FORCE)
+set(re-cmake_DOWNLOAD_URL_HASH "SHA256=e67703f83ec327e7086cbcf29a7d5548fc5580bc5c68f4c1ae4657671d5e5fc7" CACHE STRING "re-cmake download url hash" FORCE)
 
 # Uncomment/set to proper value if local instead of remote
 set(re-cmake_ROOT_DIR "") # use remote re-cmake
@@ -17,6 +18,7 @@ set(re-cmake_ROOT_DIR "") # use remote re-cmake
 set(re-logging_GIT_REPO "https://github.com/pongasoft/re-logging" CACHE STRING "re-logging git repository url")
 set(re-logging_GIT_TAG "v1.0.1" CACHE STRING "re-logging git tag")
 set(re-logging_DOWNLOAD_URL "${re-logging_GIT_REPO}/archive/refs/tags/${re-logging_GIT_TAG}.zip" CACHE STRING "re-logging download url" FORCE)
+set(re-logging_DOWNLOAD_URL_HASH "SHA256=e71898bfb4234505e5714a726139ad21ac0bd17d63f41af80d7cc9b5760dd57f" CACHE STRING "re-logging download url hash" FORCE)
 
 # Uncomment/set to proper value if local instead of remote
 set(re-logging_ROOT_DIR "") # use remote re-logging
@@ -26,22 +28,21 @@ set(re-logging_ROOT_DIR "") # use remote re-logging
 # re-mock
 #################
 set(re-mock_GIT_REPO "https://github.com/pongasoft/re-mock" CACHE STRING "re-mock git repository url")
-set(re-mock_GIT_TAG "v1.4.1" CACHE STRING "re-mock git tag")
+set(re-mock_GIT_TAG "v1.4.2" CACHE STRING "re-mock git tag")
 set(re-mock_DOWNLOAD_URL "${re-mock_GIT_REPO}/archive/refs/tags/${re-mock_GIT_TAG}.zip" CACHE STRING "re-mock download url" FORCE)
+set(re-mock_DOWNLOAD_URL_HASH "SHA256=f19e41cde101334e5c99adebba17d23eed08aa3039120d6c0794402f3ee491b8" CACHE STRING "re-mock download url hash" FORCE)
 
 # Uncomment/set to proper value if local instead of remote
 set(re-mock_ROOT_DIR "") # use remote re-mock
 #set(re-mock_ROOT_DIR "${RE_PROJECT_ROOT_DIR}/../../pongasoft/re-mock")
 
-# Set to ON if want to load/save samples with re-mock
-option(RE_MOCK_SUPPORT_FOR_AUDIO_FILE "" [-options_re_mock_support_for_audio_file-])
-
 #################
 # googletest
 #################
 set(googletest_GIT_REPO "https://github.com/google/googletest" CACHE STRING "googletest git repository URL")
-set(googletest_GIT_TAG "release-1.12.1" CACHE STRING "googletest git tag")
+set(googletest_GIT_TAG "v1.13.0" CACHE STRING "googletest git tag")
 set(googletest_DOWNLOAD_URL "${googletest_GIT_REPO}/archive/refs/tags/${googletest_GIT_TAG}.zip" CACHE STRING "googletest download url" FORCE)
+set(googletest_DOWNLOAD_URL_HASH "SHA256=ffa17fbc5953900994e2deec164bb8949879ea09b411e07f215bfbb1f87f4632" CACHE STRING "googletest download url hash" FORCE)
 
 # Uncomment/set to proper value if local instead of remote
 set(googletest_ROOT_DIR "") # use remote googletest
